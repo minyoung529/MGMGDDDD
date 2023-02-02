9804b3ecbbcb72fcafbd25d1239e085c8f84364e
Namsojeong <nsj050320@gmail.com>
2023-02-02 14:05:24 +0900

    23-02-02 AimCamera Limit 버그 수정

--- Assets/01.Scripts/Camera/ThirdPersonCameraControll.cs
+++ Assets/01.Scripts/Camera/ThirdPersonCameraControll.cs
@@ -21,8 +21,8 @@ public class ThirdPersonCameraControll : MonoBehaviour
 
     private const float rotationSpeed = 10.0f; // 회전 속도
 
-    private const float limitMinX = -90; // 카메라 y축 회전 범위 (최소)
-    private const float limitMaxX = 90; // 카메라 y축 회전 범위 (최대)
+    private const float limitMinX = -80; // 카메라 y축 회전 범위 (최소)
+    private const float limitMaxX = 80; // 카메라 y축 회전 범위 (최대)
 
     private float eulerAngleX; // 마우스 좌 / 우 이동으로 카메라 y축 회전
     private float eulerAngleY; // 마우스 위 / 아래 이동으로 카메라 x축 회전
@@ -143,16 +143,6 @@ public class ThirdPersonCameraControll : MonoBehaviour
     // 카메라 x축 회전의 경우 회전 범위를 설정
     private float ClampAngle(float angle, float min, float max)
     {
-        if (angle < -360)
-        {
-            angle += 360;
-        }
-
-        if (angle > 360)
-        {
-            angle -= 360;
-        }
-
         return Mathf.Clamp(angle, min, max);
     }
 
