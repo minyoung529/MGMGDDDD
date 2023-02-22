eb8c35a780fa0f92d67656b2a7244ff8153474de
minyoung529 <dlalsdud102613@gmail.com>
2023-02-22 23:08:58 +0900

    기어 애니메이션 적용

---
+++ Assets/01.Scripts/Function/Animation/GearAnimation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearAnimation : MonoBehaviour
+{
+    private List<Animator> animators;
+
+    private void Start()
+    {
+        animators = new List<Animator>(transform.GetComponentsInChildren<Animator>());
+    }
+
+    public void Rotate()
+    {
+        foreach(Animator animator in animators)
+        {
+            animator.SetBool("Rotate", true);
+        }
+    }
+
+    public void StopRotate()
+    {
+        foreach (Animator animator in animators)
+        {
+            animator.SetBool("Rotate", false);
+        }
+    }
+}
