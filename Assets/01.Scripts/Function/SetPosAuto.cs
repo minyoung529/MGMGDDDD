60f44fe4dc13b8f45eb96951d921ae474b4cc940
qpwo0258 <77845353+qpwo0258@users.noreply.github.com>
2023-03-07 14:31:09 +0900

    SetPosAuto버그 수정

--- Assets/01.Scripts/Function/SetPosAuto.cs
+++ Assets/01.Scripts/Function/SetPosAuto.cs
@@ -1,38 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SetPosAuto : MonoBehaviour
 {
     [SerializeField] private List<Transform> targetTransform;
     [SerializeField] private float distance = 1f;
-    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float moveTime = 1f;
     private bool isSet = false;
 
     private void Update() {
         if (isSet) return;
-        foreach (Transform item in targetTransform)
-        {
-            if ((item.position - transform.position).sqrMagnitude <= Mathf.Pow(distance, 2))
-            {
-                StartCoroutine(Move(item));
-                isSet = true;
+        float min = 0;
+        Transform target = null;
+        foreach (Transform item in targetTransform) {
+            float distance = (item.position - transform.position).sqrMagnitude;
+            if (distance <= Mathf.Pow(this.distance, 2)) {
+                if (min == 0 || distance < min)
+                    target = item;
             }
         }
+        if (!target) return;
+        transform.DOMove(target.position, moveTime);
+        transform.DORotate(target.rotation.eulerAngles, moveTime);
+        isSet = true;
     }
 
-    private IEnumerator Move(Transform pos) {
-        while((pos.position - transform.position).sqrMagnitude <= 0.01f) {
-            transform.position = Vector3.MoveTowards(transform.position, pos.position, moveSpeed * Time.deltaTime);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, pos.rotation, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        transform.position = pos.position;
-        transform.rotation = pos.rotation;
-    }
-
-    public void Reset()
-    {
+    public void Reset() {
         isSet = false;
     }
 } 
