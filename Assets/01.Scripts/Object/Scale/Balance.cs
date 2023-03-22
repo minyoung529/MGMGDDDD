30338863f05e870715fb8b323a7582706394a062
Namsojeong <nsj050320@gmail.com>
2023-03-22 20:02:28 +0900

    23-03-22 저울 완성

--- Assets/01.Scripts/Object/Scale/Balance.cs
+++ Assets/01.Scripts/Object/Scale/Balance.cs
@@ -6,46 +6,49 @@ using UnityEngine;
 public class Balance : MonoBehaviour
 {
     [SerializeField] private float maxRotation = 140f;
-    [SerializeField] private float minRotation = 50f;
-    [SerializeField] private float rotationTime = 0.5f;
+    [SerializeField] private float minRotation = 0f;
+    [SerializeField] private float rotationTime = 0.7f;
 
     [SerializeField] private BalanceFloor leftBalance;
     [SerializeField] private BalanceFloor rightBalance;
 
-    [SerializeField] private Transform leftAnchor;
-    [SerializeField] private Transform rightAnchor;
     [SerializeField] private Transform leftPillar;
     [SerializeField] private Transform rightPillar;
-
+    
+    private Transform leftAnchor;
+    private Transform rightAnchor;
     private Transform pillar;
 
     private void Awake()
     {
         pillar = transform.GetChild(0);
+        leftAnchor = pillar.GetChild(0);
+        rightAnchor = pillar.GetChild(1);
     }
 
-    private void Start()
+    private void FixedUpdate()
     {
-        Debug.Log(leftBalance.GetWeight);
-        Debug.Log(rightBalance.GetWeight);
+        leftPillar.position = leftAnchor.position;
+        rightPillar.position = rightAnchor.position;
     }
 
     public void CompareWeight()
     {
         float val = rightBalance.GetWeight - leftBalance.GetWeight;
+
         RotateWeight(val);
     }
 
     private void RotateWeight(float value)
     {
-        float xVal = Mathf.Clamp(value, minRotation, maxRotation);
-        Quaternion q = Quaternion.Euler(transform.rotation.x, transform.rotation.y, xVal);
+        Quaternion origin = Quaternion.Euler(pillar.rotation.x, pillar.rotation.y, value);
+        Quaternion q = Quaternion.Euler(pillar.rotation.x, pillar.rotation.y, value + 0.3f);
 
         pillar.DOKill();
-        pillar.DOLocalRotateQuaternion(q, rotationTime);
-
-        leftPillar.DOMove(leftAnchor.position, rotationTime);
-        rightPillar.DOMove(rightAnchor.position, rotationTime);
+        pillar.DOLocalRotateQuaternion(q, rotationTime).OnComplete(()=>
+        {
+            pillar.DOLocalRotateQuaternion(origin, rotationTime);
+        });
     }
    
 }
