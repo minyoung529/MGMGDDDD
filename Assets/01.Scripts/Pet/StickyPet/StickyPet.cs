c10fed06688cbfad590419ab92d2cd4dc939838c
Namsojeong <nsj050320@gmail.com>
2023-03-24 00:48:06 +0900

    23-03-24 StickyPet kinematic 예외처리 + 미로 퍼즐 구현 중

--- Assets/01.Scripts/Pet/StickyPet/StickyPet.cs
+++ Assets/01.Scripts/Pet/StickyPet/StickyPet.cs
@@ -13,20 +13,22 @@ public enum StickyState
 public class StickyPet : Pet
 {
     [SerializeField] private ParticleSystem skillEffect;
-
-    private float moveSpeed = 1f;
+    [SerializeField] private Transform scaleObject;
 
     private StickyState state = StickyState.Idle;
     private Vector3 bigScale = new Vector3(3f, 3f, 3f);
     private Vector3 smallDirection;
 
+    private float moveSpeed = 1f;
+    
     [SerializeField]
     private UnityEvent OnBillow;
     [SerializeField]
     private UnityEvent OnExitBillow;
 
-    [SerializeField]
-    private Transform scaleObject;
+    private Sticky stickyObject = null;
+    private bool stickyKinematic = false;
+
 
     protected override void Awake()
     {
@@ -48,6 +50,7 @@ public class StickyPet : Pet
 
         NotSticky();
         skillEffect.Play();
+        stickyObject = null;
         ChangeState(StickyState.Idle);
 
         scaleObject.DOScale(Vector3.one, 0.5f);
@@ -70,7 +73,6 @@ public class StickyPet : Pet
         CanMove = canMove;
         agent.enabled = canMove;
     }
-
     private void ChangeState(StickyState setState)
     {
         state = setState;
@@ -135,27 +137,41 @@ public class StickyPet : Pet
         }
     }
 
-    private void Sticky(Sticky stickyObject)
+    private void Sticky(Sticky sticky)
     {
         if (state == StickyState.Sticky) return;
         ChangeState(StickyState.Sticky);
 
+        stickyObject = sticky;
         skillEffect.Play();
         Rigid.isKinematic = true;
         SetMove(stickyObject.CanMove);
 
-        FixedJoint joint = gameObject.AddComponent<FixedJoint>();
-        joint.connectedBody = stickyObject.GetComponent<Rigidbody>();
+        stickyKinematic = stickyObject.GetComponent<Rigidbody>().isKinematic;
+        if (stickyKinematic)
+        {
+            stickyObject.transform.SetParent(transform);
+        }
+        else
+        {
+            FixedJoint joint = gameObject.AddComponent<FixedJoint>();
+            joint.connectedBody = stickyObject.GetComponent<Rigidbody>();
+        }
     }
     private void NotSticky()
     {
         ChangeState(StickyState.Idle);
 
-        FixedJoint[] joints = GetComponents<FixedJoint>();
-        for (int i = 0; i < joints.Length; i++)
+        if(stickyKinematic) stickyObject.transform.SetParent(null);
+        else
         {
-            Destroy(joints[i]);
+            FixedJoint[] joints = GetComponents<FixedJoint>();
+            for (int i = 0; i < joints.Length; i++)
+            {
+                Destroy(joints[i]);
+            }
         }
+       
         SetMove(true);
 
         skillEffect.Play();
