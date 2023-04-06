0a33e02823fdb9974e8c0361cc9a284fe5d48d88
Namsojeong <nsj050320@gmail.com>
2023-04-06 10:12:02 +0900

    23-04-06 돌아가는 맵과 State 상태 만드는 중

--- Assets/01.Scripts/DialPuzzle/DialPuzzleController.cs
+++ Assets/01.Scripts/DialPuzzle/DialPuzzleController.cs
@@ -1,21 +1,73 @@
+using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TimeType
+{
+    None = 0,
+
+    Morning = 1,
+    Afternoon = 2,
+    Evening = 3,
+    Night = 4,
+}
+
 public class DialPuzzleController : MonoBehaviour
 {
-    /*
-    - 힌트 문장
-    
-    ⇒ (아침 낮 저녁 밤) 정답 저장
-    
-    - 순서대로 답 저장하기
-    - 돌아간 시간 상태
-    - 다이얼 구멍 줄어드는 시간
-    - 다이얼 시작과 종료
-    */
+    [SerializeField] private float lessTimer = 30f;
+    [SerializeField] private string hintString = "";
+    [SerializeField] private GameObject map;
 
-    private string hintString = "";
-    //private 
+    private Queue<TimeType> answer = new Queue<TimeType>();
+    private TimeType curState = TimeType.None;
 
+    public string Hint => hintString;
+    public TimeType CurState => curState;
+
+    #region Answer
+    public void InputAnswer(TimeType ans)
+    {
+        answer.Enqueue(ans);
+    }
+    #endregion
+
+    #region State
+    [ContextMenu("ChangeState")]
+    public void Test()
+    {
+        ChangeState(TimeType.Evening);
+        Debug.Log((TimeType)((int)curState + 1));
+    }
+    public void ChangeState(TimeType state)
+    {
+        Debug.Log((int)state);
+        Vector3 rot = new Vector3(0, (int)state * 90f, 0);
+        map.transform.DORotate(rot, 0.5f).OnComplete(() =>
+            {
+                CheckCurrentState();
+            });
+
+    }
+    private void CheckCurrentState()
+    {
+        double stateValue = Math.Truncate(map.transform.rotation.y / 90);
+        Debug.Log(stateValue);
+        curState = (TimeType)((int)stateValue);
+    }
+
+
+    #endregion
+
+    #region Start/Stop
+    public void StartDialPuzzle()
+    {
+
+    }
+    public void StopDialPuzzle()
+    {
+
+    }
+    #endregion
 }
