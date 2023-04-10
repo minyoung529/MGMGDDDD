4627737022d853e0b7a0b1c7d8d8f54181c363b4
Namsojeong <nsj050320@gmail.com>
2023-04-11 01:22:07 +0900

    23-04-11 Dial Puzzle MorningTime 기능 구현은 완료(다듬어야 함)

--- Assets/01.Scripts/Puzzle/DialPuzzle/Morning/MorningDialPuzzle.cs
+++ Assets/01.Scripts/Puzzle/DialPuzzle/Morning/MorningDialPuzzle.cs
@@ -37,7 +37,6 @@ public class MorningDialPuzzle : MonoBehaviour
         while(true)
         {
             yield return new WaitForSeconds(Random.Range(0.5f, 3f));
-            Debug.Log("ASD");
             InstantiateCar();
         }
     }
@@ -91,4 +90,10 @@ public class MorningDialPuzzle : MonoBehaviour
     // 2. 차가 앞으로 지나감
     //    차가 일정 거리 이상 나가면 Return
     // 3. 만약 플레이어랑 부딪히면 GameOver
+
+    // 예외
+    // 스폰 시 ::
+    // 일정 거리 이하면 앞에 차보다 느려야 함 뚫고 추월X
+    // 방금 스폰된 곳이 아니어야 함
+    // 여러 곳에 동시에 출발 가능
 }
