a99f103f44bf3b8a62efc35a07b98131f885500a
Namsojeong <nsj050320@gmail.com>
2023-04-11 15:01:23 +0900

    23-04-11 신호등 구현

--- Assets/01.Scripts/Puzzle/DialPuzzle/Morning/DialTrain.cs
+++ Assets/01.Scripts/Puzzle/DialPuzzle/Morning/DialTrain.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class DialTrain : MonoBehaviour
 {
+    [Header("TrafficLight")]
+    [SerializeField] Light redLight;
+    [SerializeField] Light greenLight;
+
+    [Header("Value")]
     [SerializeField] float driveTime = 3.0f;
     [SerializeField] float warningTime = 2.0f;
     [SerializeField] float minDriveCoolTime = 3.0f;
@@ -12,6 +17,7 @@ public class DialTrain : MonoBehaviour
 
     private Vector3 startPos = new Vector3(0.015f, 1f, 0.25f);
     private float endPosZ = 0.7f;
+    private bool stop = false;
 
     private void Start()
     {
@@ -30,14 +36,45 @@ public class DialTrain : MonoBehaviour
     IEnumerator Drive()
     {
         float randomCoolTime = Random.Range(minDriveCoolTime, maxDriveCoolTime);
+        SwitchWarning(true);
         yield return new WaitForSeconds(randomCoolTime);
         StartCoroutine(Warning());
     }
     
     IEnumerator Warning()
     {
-        // Sound & Light
+        // Sound
         yield return new WaitForSeconds(warningTime);
+        SwitchWarning(false);
         DriveTrain();
+    }
+
+    private void SwitchWarning()
+    {
+        stop = !stop;
+        if(stop)
+        {
+            redLight.gameObject.SetActive(true);
+            greenLight.gameObject.SetActive(false);
+        }
+        else
+        {
+            redLight.gameObject.SetActive(false);
+            greenLight.gameObject.SetActive(true);
+        }
+    }
+    private void SwitchWarning(bool _stop)
+    {
+        stop = _stop;
+        if(stop)
+        {
+            redLight.gameObject.SetActive(true);
+            greenLight.gameObject.SetActive(false);
+        }
+        else
+        {
+            redLight.gameObject.SetActive(false);
+            greenLight.gameObject.SetActive(true);
+        }
     }
 }
