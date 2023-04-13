321bb753de95e9eb49b7fcb51a826ce657c37ed5
qpwo0258 <77845353+qpwo0258@users.noreply.github.com>
2023-04-13 20:25:03 +0900

    구멍 기능 구현

--- Assets/01.Scripts/DialPuzzle/HoleScript.cs
+++ Assets/01.Scripts/DialPuzzle/HoleScript.cs
@@ -3,52 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HoleScript : MonoBehaviour {
-    [Header("Ground")]
-    [SerializeField] private PolygonCollider2D ground2DCollider;
-    [SerializeField] private int angleAmount = 20;
-    [SerializeField] private int radius = 25;
+    [SerializeField] private float holeOriginRadius = 10f;
 
-    public PolygonCollider2D hole2DCollider;
-    public MeshCollider GeneratedMeshCollider;
-    private Mesh GeneratedMesh;
+    private List<int> vertexIndex = new List<int>();
+    private List<Vector3> direction = new List<Vector3>();
+
+    private MeshFilter meshFilter;
+    private MeshCollider meshCollider;
+    private Mesh mesh;
+
+    [SerializeField] [Range(0f, 0.48f)] private float radius = 0.1f;
 
     private void Start() {
-        Vector2[] pos = new Vector2[angleAmount];
-        pos[0] = Vector2.up * radius;
-        float angle = 360 / angleAmount;
-        for (int i = 1; i < angleAmount; i++)
-            pos[i] = Quaternion.AngleAxis(angle, Vector3.forward) * pos[i - 1];
-        ground2DCollider.points = pos;
-        SetCollider();
+        meshFilter = GetComponent<MeshFilter>();
+        meshCollider = GetComponent<MeshCollider>();
+        mesh = meshFilter.mesh;
+        for (int i = 0; i < mesh.vertexCount; i++)
+            if (mesh.vertices[i].sqrMagnitude <= Mathf.Pow(holeOriginRadius, 2)) {
+                vertexIndex.Add(i);
+                Vector3 dir = mesh.vertices[i];
+                dir.y = 0;
+                dir = dir.normalized;
+                direction.Add(dir);
+            }
     }
 
-    private void FixedUpdate() {
-        if (transform.hasChanged) {
-            transform.hasChanged = false;
-            SetCollider();
+    private void Update() {
+        SetHoleSize(radius);
+    }
+
+    private void SetHoleSize(float radius) {
+        Vector3[] vertex = mesh.vertices;
+        for (int i = 0; i < vertexIndex.Count; i++) {
+            vertex[vertexIndex[i]] = (direction[i] * radius) + (Vector3.up * vertex[vertexIndex[i]].y);
         }
-    }
-
-    private void SetCollider() {
-        hole2DCollider.transform.position = new Vector2(transform.position.x, transform.position.z);
-        hole2DCollider.transform.localScale = transform.localScale;
-
-        MakeHole2D();
-        Make3DMeshCollider();
-    }
-
-    private void MakeHole2D() {
-        Vector2[] PointPositions = hole2DCollider.GetPath(0);
-        for (int i = 0; i < PointPositions.Length; i++) {
-            PointPositions[i] = hole2DCollider.transform.TransformPoint(PointPositions[i]);
-        }
-        ground2DCollider.pathCount = 2;
-        ground2DCollider.SetPath(1, PointPositions);
-    }
-
-    private void Make3DMeshCollider() {
-        if (GeneratedMesh != null) Destroy(GeneratedMesh);
-        GeneratedMesh = ground2DCollider.CreateMesh(true, true);
-        GeneratedMeshCollider.sharedMesh = GeneratedMesh;
+        mesh.vertices = vertex;
+        meshCollider.sharedMesh = mesh;
     }
 }
