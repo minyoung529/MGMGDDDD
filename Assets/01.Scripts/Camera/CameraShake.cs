57ff5d929ac98dbb294c98d94f765d439d1fcffe
Namsojeong <nsj050320@gmail.com>
2023-04-25 20:25:27 +0900

    23-04-25 다이얼 2라운드 난이도 상승 카메라 Shake

---
+++ Assets/01.Scripts/Camera/CameraShake.cs
@@ -0,0 +1,46 @@
+using Cinemachine;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    [SerializeField] private float duration = 2f;
+    [SerializeField] private bool loop = false;
+
+    private Transform camera => CameraSwitcher.activeCamera.transform;
+
+    public void Shake(float _duration)
+    {
+        camera.DOShakePosition(_duration);
+    }
+    public void Shake()
+    {
+        camera.DOShakePosition(duration);
+    }
+    public void ShakeLooping()
+    {
+        StartCoroutine(Shaking());
+    }
+
+    public IEnumerator Shaking()
+    {
+        while(loop)
+        {
+            yield return new WaitForSeconds(0.25f);
+            camera.DOShakePosition(duration, 0.7f);
+        }
+    }
+   
+    
+    public void SetLoop(bool value)
+    {
+        loop = value;
+    }
+
+    public void StopShake()
+    {
+        camera.DOKill();
+    }
+}
