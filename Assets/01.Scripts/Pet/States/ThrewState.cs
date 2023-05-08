93443f51ef74791946212492e59d9be24363a3a5
qpwo0258 <77845353+qpwo0258@users.noreply.github.com>
2023-05-09 01:39:57 +0900

    Recall 기능 버그 수정 및 추락 시 Recall 추가

--- Assets/01.Scripts/Pet/States/ThrewState.cs
+++ Assets/01.Scripts/Pet/States/ThrewState.cs
@@ -10,21 +10,30 @@ public class ThrewState : PetState {
         pet.Rigid.velocity = Vector3.zero;
         pet.Rigid.isKinematic = false;
         pet.IsInputLock = false;
+
         StartCoroutine(EnableColl(pet.transform.position));
 
         pet.Event.StartListening((int)PetEventName.OnLanding, OnLanding);
+        pet.Event.StartListening((int)PetEventName.OnRecallKeyPress, OnRecall);
     }
 
     public override void OnExit() {
         pet.Event.StopListening((int)PetEventName.OnLanding, OnLanding);
+        pet.Event.StartListening((int)PetEventName.OnRecallKeyPress, OnRecall);
     }
 
     public override void OnUpdate() {
-
+        if(pet.transform.position.y <= -100f) {
+            pet.Event.TriggerEvent((int)PetEventName.OnRecallKeyPress);
+        }
     }
 
     private void OnLanding() {
         pet.State.ChangeState((int)PetStateName.Landing);
+    }
+
+    private void OnRecall() {
+        pet.State.ChangeState((int)PetStateName.Recall);
     }
 
     /// <summary>
