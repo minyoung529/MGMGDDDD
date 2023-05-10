1701029b5cceff9a2c3834e3c60c3bac0f891710
Namsojeong <nsj050320@gmail.com>
2023-05-10 09:17:36 +0900

    23-05-01 Interact State 구현 완료

--- Assets/01.Scripts/Pet/States/InteracterState/InteractState.cs
+++ Assets/01.Scripts/Pet/States/InteracterState/InteractState.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,46 @@ public class InteractState : PetState
 {
     public override PetStateName StateName => PetStateName.Interact;
 
+    private float moveSpeed = 1f;
+
     public override void OnEnter()
     {
+        MoveArrived();
     }
 
     public override void OnExit()
     {
+
     }
 
     public override void OnUpdate()
     {
+    }
+    private void MoveArrived()
+    {
+        Vector3 hit = GameManager.Instance.GetCameraHit();
+        if (hit != Vector3.zero)
+        {
+            transform.DOMove(hit, moveSpeed).OnComplete(() =>
+            {
+                CheckAroundInteract();
+            });
+        }
+    }
+
+
+    private void CheckAroundInteract()
+    {
+        Collider[] cols = Physics.OverlapSphere(transform.position, 3f);
+        foreach (Collider col in cols)
+        {
+            OutlineScript interact = col.GetComponent<OutlineScript>();
+            if (interact == null) continue;
+
+            interact.OnInteract();
+            pet.State.ChangeState((int)PetStateName.Idle);
+            return;
+        }
+
     }
 }
