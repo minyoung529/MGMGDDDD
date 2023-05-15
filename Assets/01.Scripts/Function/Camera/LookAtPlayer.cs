73a5cd9d5a8c76dd1c0c7996fbee7ec98d507b7a
minyoung529 <dlalsdud102613@gmail.com>
2023-05-15 18:51:10 +0900

    불꽃놀이 Billboard 기능 추가

--- Assets/01.Scripts/Function/Camera/LookAtPlayer.cs
+++ Assets/01.Scripts/Function/Camera/LookAtPlayer.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class LookAtPlayer : MonoBehaviour
 {
+    [SerializeField]
+    private float duration = 0f;
+    private float timer = 0f;
     private Transform player;
 
     private void Start()
     {
-        player = GameManager.Instance.PlayerController.transform;
+        player = GameManager.Instance.MainCam.transform;
+        timer = duration;
+    }
+
+    private  void Update()
+    {
+        if (timer < duration)
+        {
+            timer += Time.deltaTime;
+            transform.forward = -player.transform.forward;
+        }
     }
 
     public void LookAt()
     {
-        if(player == null)
-        {
-            Debug.Log("SDf");
-            return;
-        }
-    
-        transform.LookAt(player.transform, Vector3.up);
+        timer = 0f;
     }
 }
