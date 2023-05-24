5f3b87de9e37c1c8db7b86f233e9a2e0cf1205de
qpwo0258 <77845353+qpwo0258@users.noreply.github.com>
2023-05-25 00:14:36 +0900

    펫 관련 코드들 약간 수정

--- Assets/01.Scripts/Pet/Enum/PetStateName.cs
+++ Assets/01.Scripts/Pet/Enum/PetStateName.cs
@@ -7,6 +7,7 @@ public enum PetStateName
     Threw,
     Landing,
     Recall,
+    Pause,
     Sticky,
     Length
 }
---
+++ Assets/01.Scripts/Pet/States/PuaseState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuaseState : PetState {
+    public override PetStateName StateName => PetStateName.Pause;
+
+    public override void OnEnter() {
+
+    }
+
+    public override void OnExit() {
+
+    }
+
+    public override void OnUpdate() {
+
+    }
+}
