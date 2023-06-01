f4fab381e3d693bf049f407477f1a94260ae7333
minyoung529 <dlalsdud102613@gmail.com>
2023-06-01 17:55:05 +0900

    Add Cube Pushing Sound

---
+++ Assets/01.Scripts/Function/Interaction/PushedObject.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PushedObject : MonoBehaviour
+{
+    private bool isContactPlayer = false;
+    private bool isMove = false;
+
+    [SerializeField]
+    private UnityEvent onPushStart;
+
+    [SerializeField]
+    private UnityEvent onPushEnd;
+
+    private Vector3 prevPosition;
+
+    void Start()
+    {
+        prevPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        bool prevMove = isMove;
+        isMove = ((prevPosition - transform.position).sqrMagnitude > 0.001f);
+        prevPosition = transform.position;
+        
+        if (isContactPlayer)
+        {
+            if (prevMove != isMove)
+            {
+                if (isMove)
+                {
+                    onPushStart?.Invoke();
+                }
+                else
+                {
+                    onPushEnd?.Invoke();
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag(Define.PLAYER_TAG))
+        {
+            isContactPlayer = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag(Define.PLAYER_TAG))
+        {
+            isContactPlayer = false;
+        }
+    }
+}
