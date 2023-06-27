f5b77320f67dfddab835c7fb2a425a00031c8790
Namsojeong <nsj050320@gmail.com>
2023-06-28 00:15:57 +0900

    퍼즐 후반 Sound 추가

--- Assets/01.Scripts/Player/DieTrigger.cs
+++ Assets/01.Scripts/Player/DieTrigger.cs
@@ -3,17 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DieTrigger : MonoBehaviour
 {
+    [SerializeField] UnityEvent onDieEvent;
     public Action OnDie { get; set; }
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag(Define.PLAYER_TAG))
         {
             EventParam param = new();
             EventManager.TriggerEvent(EventName.PlayerDie, param);
+            onDieEvent?.Invoke();
             OnDie?.Invoke();
         }
     }
@@ -24,6 +26,7 @@ public class DieTrigger : MonoBehaviour
         {
             EventParam param = new();
             EventManager.TriggerEvent(EventName.PlayerDie, param);
+            onDieEvent?.Invoke();
             OnDie?.Invoke();
         }
     }
